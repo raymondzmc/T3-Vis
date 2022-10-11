b5feafa77ead0330196604b3d394fea269409cbe
Raymond Li <li.raymond96@gmail.com>
2022-10-10 22:36:33 -0700

    xsum

--- application/run_abs_sum.py
+++ application/run_abs_sum.py
@@ -66,109 +66,105 @@ def main(args):
     cross_attn_position_count = torch.zeros((max_output_len, max_input_len), device=device)
 
 
-    for i, example in enumerate(tqdm(dataset)):
-        article = example['document']
-        highlights = example['summary']
-        _id = example['id']
-        batch = tokenizer([article], truncation=True, padding="longest", return_tensors="pt").to(device)
-        batch['max_length'] = max_output_len
-        batch['return_dict_in_generate'] = True
-        batch['output_attentions'] = True
-        batch['output_hidden_states'] = True
-        # batch['output_scores'] = True
+    # for i, example in enumerate(tqdm(dataset)):
+    #     article = example['document']
+    #     highlights = example['summary']
+    #     _id = example['id']
+    #     batch = tokenizer([article], truncation=True, padding="longest", return_tensors="pt").to(device)
+    #     batch['max_length'] = max_output_len
+    #     batch['return_dict_in_generate'] = True
+    #     batch['output_attentions'] = True
+    #     batch['output_hidden_states'] = True
+    #     # batch['output_scores'] = True
 
-        with torch.no_grad():
-            output = model.generate(**batch)
+    #     with torch.no_grad():
+    #         output = model.generate(**batch)
 
-        input_len = len(batch['input_ids'][0])
-        output_len = len(output.cross_attentions)
+    #     input_len = len(batch['input_ids'][0])
+    #     output_len = len(output.cross_attentions)
 
-    #   output_ids = output['sequences']
+    # #   output_ids = output['sequences']
 
-    #   # # Output vocab probability distribution for each generation step (removed to save memory)
-    #   # # scores = output.scores
+    # #   # # Output vocab probability distribution for each generation step (removed to save memory)
+    # #   # # scores = output.scores
 
-        if batch['output_attentions']:
-            # n_layer x batch_size x n_heads x input_len x input_len
-            encoder_attention = output.encoder_attentions
-            encoder_attention= torch.stack(encoder_attention).squeeze(1).to(dtype=torch.float16) # n_layer x n_heads x input_len x input_len 
+    #     if batch['output_attentions']:
+    #         # n_layer x batch_size x n_heads x input_len x input_len
+    #         encoder_attention = output.encoder_attentions
+    #         encoder_attention= torch.stack(encoder_attention).squeeze(1).to(dtype=torch.float16) # n_layer x n_heads x input_len x input_len 
 
-            # output_len x n_layers x beam_size x n_heads x 1 x output_len
-            decoder_attention = output.decoder_attentions
-            decoder_attention = [torch.stack(attn)[:, 0].squeeze(2).to(dtype=torch.float16) for attn in decoder_attention] # output_len x n_layers x n_heads x n_tokens_prior
+    #         # output_len x n_layers x beam_size x n_heads x 1 x output_len
+    #         decoder_attention = output.decoder_attentions
+    #         decoder_attention = [torch.stack(attn)[:, 0].squeeze(2).to(dtype=torch.float16) for attn in decoder_attention] # output_len x n_layers x n_heads x n_tokens_prior
 
-            # output_len x n_layers x beam_size x n_heads x 1 x input_size
-            cross_attention = output.cross_attentions
-            cross_attention = torch.stack([torch.stack(attn)[:, 0].squeeze(2) for attn in cross_attention]).to(dtype=torch.float16)
-            cross_attention = cross_attention.transpose(0, 1).transpose(1, 2)
-
-
-            aggregate_encoder_attn[:, :, :input_len, :input_len] += encoder_attention.cpu().numpy()
-            aggregate_cross_attn[:, :, :output_len, :input_len] += cross_attention.cpu().numpy()
-            for decoder_step, row in enumerate(decoder_attention):
-                aggregate_decoder_attn[:, :, decoder_step, :row.shape[-1]] += row.cpu().numpy()
-
-            input_position_count[:input_len] += 1
-            output_position_count[:output_len] += 1
-            cross_attn_position_count[:output_len, :input_len] += 1
-
-    #   # # (1 + n_layer) x batch_size x input_len x hidden_dim
-        encoder_hidden_states = output.encoder_hidden_states[-1][0].mean(0)
-        encoder_hiddens[i] = encoder_hidden_states.half().cpu().numpy()
-
-    #   # # output_len x (1 + n_layer) x beam_size x batch_size x hidden_dim
-        decoder_hidden_states = output.decoder_hidden_states
-        decoder_len.append(len(decoder_hidden_states))
-        decoder_hiddens.append(torch.stack([hiddens[-1][0][0] for hiddens in decoder_hidden_states]).half().cpu().numpy())
-
-    #   # tgt_text = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    #   # input_tokens = tokenizer.convert_ids_to_tokens(batch['input_ids'][0])
-    #   # target_tokens = tokenizer.convert_ids_to_tokens(output_ids[0])
-
-    #   # id_list.append(_id)
-
-    torch.save(encoder_hiddens, pjoin(args.output_dir, 'encoder_hidden_states.pt'))
-    torch.save(decoder_hiddens, pjoin(args.output_dir, 'decoder_hidden_states.pt'))
+    #         # output_len x n_layers x beam_size x n_heads x 1 x input_size
+    #         cross_attention = output.cross_attentions
+    #         cross_attention = torch.stack([torch.stack(attn)[:, 0].squeeze(2) for attn in cross_attention]).to(dtype=torch.float16)
+    #         cross_attention = cross_attention.transpose(0, 1).transpose(1, 2)
 
 
+    #         aggregate_encoder_attn[:, :, :input_len, :input_len] += encoder_attention.cpu().numpy()
+    #         aggregate_cross_attn[:, :, :output_len, :input_len] += cross_attention.cpu().numpy()
+    #         for decoder_step, row in enumerate(decoder_attention):
+    #             aggregate_decoder_attn[:, :, decoder_step, :row.shape[-1]] += row.cpu().numpy()
 
-    max_input_len = len(input_position_count.nonzero(as_tuple=False))
-    max_output_len = len(output_position_count.nonzero(as_tuple=False))
-    aggregate_encoder_attn = aggregate_encoder_attn[:, :, :max_input_len, :max_input_len] / \
-                             input_position_count.cpu().numpy()[:max_input_len]
+    #         input_position_count[:input_len] += 1
+    #         output_position_count[:output_len] += 1
+    #         cross_attn_position_count[:output_len, :input_len] += 1
 
-    aggregate_decoder_attn = aggregate_decoder_attn[:, :, :max_output_len, :max_output_len] /\
-                             output_position_count.cpu().numpy()[:max_output_len]
+    # #   # # (1 + n_layer) x batch_size x input_len x hidden_dim
+    #     encoder_hidden_states = output.encoder_hidden_states[-1][0].mean(0)
+    #     encoder_hiddens[i] = encoder_hidden_states.half().cpu().numpy()
 
-    cross_attn_position_count[cross_attn_position_count == 0] = 1
-    aggregate_cross_attn = aggregate_cross_attn[:, :, :max_output_len, :max_input_len] /\
-                           cross_attn_position_count.cpu().numpy()[:max_output_len, :max_input_len]
+    # #   # # output_len x (1 + n_layer) x beam_size x batch_size x hidden_dim
+    #     decoder_hidden_states = output.decoder_hidden_states
+    #     decoder_len.append(len(decoder_hidden_states))
+    #     decoder_hiddens.append(torch.stack([hiddens[-1][0][0] for hiddens in decoder_hidden_states]).half().cpu().numpy())
 
-    torch.save(aggregate_encoder_attn, pjoin(args.output_dir, 'aggregate_encoder_attn.pt'))
-    torch.save(aggregate_decoder_attn, pjoin(args.output_dir, 'aggregate_decoder_attn.pt'))
-    torch.save(aggregate_cross_attn, pjoin(args.output_dir, 'aggregate_cross_attn.pt'))
+    # torch.save(encoder_hiddens, pjoin(args.output_dir, 'encoder_hidden_states.pt'))
+    # torch.save(decoder_hiddens, pjoin(args.output_dir, 'decoder_hidden_states.pt'))
 
-    encoder_attn_img = format_attention_image(aggregate_encoder_attn)
-    decoder_attn_img = format_attention_image(aggregate_decoder_attn)
-    cross_attn_img = format_attention_image(aggregate_cross_attn)
-    torch.save(encoder_attn_img, pjoin(args.output_dir, 'aggregate_encoder_attn_img.pt'))
-    torch.save(decoder_attn_img, pjoin(args.output_dir, 'aggregate_decoder_attn_img.pt'))
-    torch.save(cross_attn_img, pjoin(args.output_dir, 'aggregate_cross_attn_img.pt'))
 
-    fit = umap.UMAP(
-        n_neighbors=5,
-        min_dist=0.1,
-        n_components=2,
-        metric='cosine',
-        verbose=True,
-        low_memory=True,
-        init='random',
-    )
+    # max_input_len = len(input_position_count.nonzero(as_tuple=False))
+    # max_output_len = len(output_position_count.nonzero(as_tuple=False))
+    # aggregate_encoder_attn = aggregate_encoder_attn[:, :, :max_input_len, :max_input_len] / \
+    #                          input_position_count.cpu().numpy()[:max_input_len]
 
-    encoder_mean_projections = fit.fit_transform(encoder_hiddens)
-    torch.save(encoder_mean_projections, pjoin(args.output_dir, 'encoder_mean_projections.pt'))
+    # aggregate_decoder_attn = aggregate_decoder_attn[:, :, :max_output_len, :max_output_len] /\
+    #                          output_position_count.cpu().numpy()[:max_output_len]
 
-    decoder_all_hiddens = torch.cat(decoder_hiddens, 0)
+    # cross_attn_position_count[cross_attn_position_count == 0] = 1
+    # aggregate_cross_attn = aggregate_cross_attn[:, :, :max_output_len, :max_input_len] /\
+    #                        cross_attn_position_count.cpu().numpy()[:max_output_len, :max_input_len]
+
+    # torch.save(aggregate_encoder_attn, pjoin(args.output_dir, 'aggregate_encoder_attn.pt'))
+    # torch.save(aggregate_decoder_attn, pjoin(args.output_dir, 'aggregate_decoder_attn.pt'))
+    # torch.save(aggregate_cross_attn, pjoin(args.output_dir, 'aggregate_cross_attn.pt'))
+
+    # encoder_attn_img = format_attention_image(aggregate_encoder_attn)
+    # decoder_attn_img = format_attention_image(aggregate_decoder_attn)
+    # cross_attn_img = format_attention_image(aggregate_cross_attn)
+    # torch.save(encoder_attn_img, pjoin(args.output_dir, 'aggregate_encoder_attn_img.pt'))
+    # torch.save(decoder_attn_img, pjoin(args.output_dir, 'aggregate_decoder_attn_img.pt'))
+    # torch.save(cross_attn_img, pjoin(args.output_dir, 'aggregate_cross_attn_img.pt'))
+
+    # fit = umap.UMAP(
+    #     n_neighbors=5,
+    #     min_dist=0.1,
+    #     n_components=2,
+    #     metric='cosine',
+    #     verbose=True,
+    #     low_memory=True,
+    #     init='random',
+    # )
+
+    # encoder_mean_projections = fit.fit_transform(encoder_hiddens)
+    # torch.save(encoder_mean_projections, pjoin(args.output_dir, 'encoder_mean_projections.pt'))
+
+    decoder_hiddens = torch.load(pjoin(args.output_dir, 'decoder_hidden_states.pt'))
+    decoder_len = [len(h) for h in decoder_hiddens]
+
+    decoder_all_hiddens = np.concatenate(decoder_hiddens, axis=0)
     decoder_all_projections = fit.fit_transform(decoder_all_hiddens)
     _decoder_all_projections = []
     start = 0
